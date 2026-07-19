a6108c9714d3eab54ef5f1dfb0ce59bdc865478f
tanvir134338 <tanvir134338@gmail.com>
2026-07-20 05:26:07 +0600

    while loop and for loop

---
+++ for-loop.js
@@ -0,0 +1,13 @@
+/**
+ * for(initialization; condition; modification){
+ *      repeatative task.
+ * }
+ * 
+ * 
+ * 
+ * 
+ */
+
+for(let i=1; i<10; i++){
+    console.log('Value of i', i)
+}
---
+++ while.js
@@ -0,0 +1,19 @@
+/**
+ * this is the for loop
+ */
+
+
+// for(let i=599; i < 607; i++){
+//     console.log('Count',i)
+// }
+
+
+/**
+ * this is the while loop
+ */
+
+let i =598;
+while(i<=603){
+    console.log('Inside while loop',i);
+    i++;
+}
