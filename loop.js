6aca4d90af236b3b48170ac9099a18cdab599512
tanvir134338 <tanvir134338@gmail.com>
2026-07-20 04:46:35 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,23 @@
+/**
+ * 1.Starting situation 
+ * 2.Coount value adopt
+ * 3.stopping condition
+ * 4.same or similar tast repeatative
+ */
+
+
+// let count = 0;
+// count < 10;
+// count++;
+// console.log('do something')
+
+// structure //
+// -------------------------
+// for( initialization ; condition ; modification){
+//     repeatative Task.
+// }
+// ----------------------------------------------------------
+
+for(let count =0 ; count<30 ; count++){
+    console.log('This is testing')
+}
---
+++ repeat.js
@@ -0,0 +1,2 @@
+console.log('Show me something');
+
