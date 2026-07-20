c03c62c67b2ebf3952ad1283b4765547a225e3a3
tanvir134338 <tanvir134338@gmail.com>
2026-07-20 06:39:32 +0600

    do while loop

---
+++ do-while.js
@@ -0,0 +1,15 @@
+// let i =0;
+// while(i<5){
+//     console.log('Inside while loop',i);
+//     i++;
+// }
+
+/**
+ * While er khalato vai, ulta version
+ */
+
+let i= 0;
+do{
+    console.log('Value of i',i);i++
+}
+while(i>5)
