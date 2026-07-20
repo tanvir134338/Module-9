57f1a5dace5eac92eda72ea8cd2d43c815d0670a
tanvir134338 <tanvir134338@gmail.com>
2026-07-20 06:06:40 +0600

    namta banano by js

---
+++ namta.js
@@ -0,0 +1,11 @@
+// const number = 5;
+
+// for(let i=1; i<=10; i++){
+//     console.log(number,'x', i, '=', number*i)
+// }
+
+const num=7;
+
+for(let i=1; i<=10; i++){
+    console.log(num, 'x', i, '=', num*i)
+}
