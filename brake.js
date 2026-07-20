110a6903d70db280afa0b406e8886297a16131ea
tanvir134338 <tanvir134338@gmail.com>
2026-07-20 06:31:29 +0600

    brake & continue like skip

---
+++ brake.js
@@ -0,0 +1,15 @@
+// for(let i=10; i<=30; i++){
+//     if(i>18){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+let i=10;
+while(i<=30){
+    if(i>22){
+        break
+    }
+    console.log(i)
+ i=i+2;
+}
---
+++ continue.js
@@ -0,0 +1,10 @@
+// Continue is like skip//
+
+let i=1;
+while(i<30){
+    if(i===14){
+        continue
+    }
+    console.log(i)
+ i=i+2;
+}
